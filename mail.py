7cf13eadeb64a696f335ce0d7a71dbbe6df5ead8
Kevin <kxt7073@g.rit.edu>
2019-05-22 02:22:20 -0400

    Finished code

--- mail.py
+++ mail.py
@@ -1,34 +1,41 @@
 import smtplib
-import datetime
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEImage import MIMEImage
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 myEmail = "rpdb585@gmail.com"
 password = "Cabbage1!"
 
 def sendEmail(image):
-	msgRoot = MIMEMultipart('related')
-	msgRoot['Subject'] = "Doorbell Pushed"
-	msgRoot['From'] = myEmail
-	msgRoot['To'] = myEmail
-	msgRoot.preamble = datetime.datetime.now()
-	
-	msgAlternative = MIMEMultipart('alternative')
-	msgRoot.attach(msgAlternative)
-	msgText = MIMEText('Smart security cam found object')
-	msgAlternative.attach(msgText)
-	
-	msgText = MIMEText('<img src="cidd:image1">','html')
-	msgAlternative.attach(msgText)
-	
-	msgImage = MIMEImage(image)
-	msgImage.add_header('Content-ID','<image1>')
-	msgRoot.attach(msgImage)
-	
-	smtp = smtplib.SMTP('smtp.gmail.com',587)
-	smtp.starttls()
-	smtp.login(myEmail,password)
-	smtp.sendEmail(myEmail,myEmail,msgRoot.as_string())
-	smtp.quit()
-	
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = "Doorbell Pushed"
+    msgRoot['From'] = myEmail
+    msgRoot['To'] = myEmail
+    msgRoot.preamble = "test "
+    
+    #msgAlternative = MIMEMultipart('alternative')
+    #msgRoot.attach(msgAlternative)
+    #msgText = MIMEText('Smart security cam found object')
+    #msgAlternative.attach(msgText)
+    
+    #msgText = MIMEText('<img src="cidd:image1">','html')
+    #msgAlternative.attach(msgText)
+    
+    img = open(image,'rb').read()
+    image = MIMEImage(img,name=os.path.basename(image))
+    msgRoot.attach(image)
+    
+    #msgImage = MIMEImage(image)
+    #msgImage.add_header('Content-ID','<image1>')
+    #msgRoot.attach(msgImage)
+    
+    smtp = smtplib.SMTP('smtp.gmail.com',587)
+    #smtp.ehlo()
+    smtp.starttls()
+    #smtp.ehlo()
+    smtp.login(myEmail,password)
+    
+    smtp.sendmail(myEmail,myEmail,msgRoot.as_string())
+    smtp.quit()
+    
