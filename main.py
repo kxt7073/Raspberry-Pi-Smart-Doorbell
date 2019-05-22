1e160dbdf20ec4f440c3a2c9836952abb5621c23
Kevin <kxt7073@g.rit.edu>
2019-05-22 02:20:50 -0400

    Test lines

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import mail
-import camera
+from mail import sendEmail
+from camera import pic
 
 def main():
-	picture = camera.pic()
-	mail.sendEmail(picture)
-	
+    print("nihaody")
+    pic()
+    print("swamp")
+    sendEmail('/home/pi/Desktop/image.jpg')
+    print("hi")
+    
+if __name__=="__main__":
+    main()
