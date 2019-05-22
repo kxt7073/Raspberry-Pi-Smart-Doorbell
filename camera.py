3f11379f8096964bf969efb47cc55e37b9260a35
Kevin <kxt7073@g.rit.edu>
2019-05-22 02:21:57 -0400

    finalized

--- camera.py
+++ camera.py
@@ -1,9 +1,13 @@
 from picamera import PiCamera
 from time import sleep
 
-def pic()
-	camera = PiCamera()
-	camera.start_preview()
-	sleep(2.5)
-	camera.capture('/home/i/Desktop/image.jpg')
-	camera.stop_preview()
+def pic():
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(2.5)
+    picture = camera.capture('/home/pi/Desktop/image.jpg')
+    camera.stop_preview()
+    return picture
+    
+if __name__ == "__main__":
+    pic()
