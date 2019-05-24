f53cad646546e77b45c82b5b7147e59c6237742c
Kevin <kxt7073@g.rit.edu>
2019-05-24 03:42:37 -0400

    Finishing touches and added comments

--- mail.py
+++ mail.py
@@ -1,41 +1,38 @@
+"""
+    Mail program for Smart Raspberry Pi Doorbell
+    @author Kevin Ta : kevinduynta@gmail.com
+    Date 5/24/2019
+"""
 import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-myEmail = "rpdb585@gmail.com"
-password = "Cabbage1!"
+myEmail = "Your email"
+password = "Your password"
 
+#Sends an email notification to desired email
+#For this project it emails itself
+#If desired to send an email from and to a different person
+#Uncomment and add the desired email to send to
+#ToEmail = "To person email"
+#Also change Line 20 from 'myEmail' to 'ToEmail'
+#And change line 31 second instance of 'myEmail' to 'ToEmail'
 def sendEmail(image):
     msgRoot = MIMEMultipart('related')
     msgRoot['Subject'] = "Doorbell Pushed"
     msgRoot['From'] = myEmail
-    msgRoot['To'] = myEmail
+    msgRoot['To'] = myEmail #ToEmail
     msgRoot.preamble = "test "
-    
-    #msgAlternative = MIMEMultipart('alternative')
-    #msgRoot.attach(msgAlternative)
-    #msgText = MIMEText('Smart security cam found object')
-    #msgAlternative.attach(msgText)
-    
-    #msgText = MIMEText('<img src="cidd:image1">','html')
-    #msgAlternative.attach(msgText)
-    
+        
     img = open(image,'rb').read()
     image = MIMEImage(img,name=os.path.basename(image))
     msgRoot.attach(image)
     
-    #msgImage = MIMEImage(image)
-    #msgImage.add_header('Content-ID','<image1>')
-    #msgRoot.attach(msgImage)
-    
     smtp = smtplib.SMTP('smtp.gmail.com',587)
-    #smtp.ehlo()
     smtp.starttls()
-    #smtp.ehlo()
     smtp.login(myEmail,password)
-    
     smtp.sendmail(myEmail,myEmail,msgRoot.as_string())
     smtp.quit()
     
