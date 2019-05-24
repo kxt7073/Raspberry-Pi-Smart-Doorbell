4bbd050ef386e59ec73c858d5b418c77a63b7977
Kevin <kxt7073@g.rit.edu>
2019-05-24 03:42:14 -0400

    Finishing touches and comments

--- camera.py
+++ camera.py
@@ -1,13 +1,17 @@
+"""
+    Camera program for Smart Raspberry Pi Doorbell
+    @author Kevin Ta : kevinduynta@gmail.com
+    Date 5/24/2019
+"""
 from picamera import PiCamera
 from time import sleep
 
-def pic():
-    camera = PiCamera()
+#Captures a picture and saves as image.jpg
+def pic(camera):
     camera.start_preview()
     sleep(2.5)
     picture = camera.capture('/home/pi/Desktop/image.jpg')
     camera.stop_preview()
-    return picture
     
 if __name__ == "__main__":
     pic()
